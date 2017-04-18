8deef149a92a85bd4528a5a78a25f952cc77726e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-18 11:29:42 -0500

    webpack with configuration

---
+++ configuration_file/src/main.js
@@ -0,0 +1,2 @@
+console.log("Webpack is working");
+console.log(`Usando arrow functions: El cuadrado de 4 es: ${((x) => x * x)(4)}`)
---
+++ configuration_file/webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+const path = require('path')
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    }
+}
